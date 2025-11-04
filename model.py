0d0248508cdaa80c290a90093a89a4c61a488857
Rameetha Reddy <113629071+rameethareddy@users.noreply.github.com>
2025-11-04 10:00:26 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import CTransformers
